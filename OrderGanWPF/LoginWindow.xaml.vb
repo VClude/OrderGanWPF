656a854ed4a61a283660e7e5729e29726ac8dffd
VClude <vclude@gmail.com>
2018-12-18 18:23:25 +0700

    Initial

---
+++ OrderGanWPF/LoginWindow.xaml.vb
@@ -0,0 +1,30 @@
+﻿Imports MahApps.Metro.Controls
+Imports MahApps.Metro.Controls.Dialogs
+Imports System.Net
+Imports System.Text
+Imports System.IO
+
+Class LoginWindow
+    Inherits MetroWindow
+
+    Private Sub btLogin_Click(sender As Object, e As RoutedEventArgs) Handles btLogin.Click
+
+    End Sub
+
+    Private Async Sub btCancel_Click(sender As Object, e As RoutedEventArgs) Handles btCancel.Click
+        Lowi.Width = 357
+        Belur.Radius = 5
+        Dim result As Integer = Await ShowMessageAsync("Exit", "Are You sure ?", MessageDialogStyle.AffirmativeAndNegative)
+        If result = MessageDialogResult.Affirmative Then
+            End
+        ElseIf result = MessageDialogResult.Negative Then
+            Lowi.Width = 272
+            Belur.Radius = 0
+
+        End If
+    End Sub
+
+    Private Sub LoginWindow_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
+
+    End Sub
+End Class
---
+++ OrderGanWPF/var.vb
@@ -0,0 +1,8 @@
+﻿Module var
+    Public server, serverapi As String
+    Sub Srvaddr()
+        server = "http://125.166.114.8"
+        serverapi = "http://125.166.114.8:8000"
+    End Sub
+End Module
+
